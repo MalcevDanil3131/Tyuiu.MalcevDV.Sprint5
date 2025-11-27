b6669606f7ddca80d376b94f789311aeb64378f6
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-11-28 00:22:23 +0500

    Task3

---
+++ Tyuiu.MalcevDV.Sprint5.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MalcevDV.Sprint5.Task3.V18.Lib;
+namespace Tyuiu.MalcevDV.Sprint5.Task3.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\danil\AppData\Local\Temp\OutPutFileTask3.bin";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.MalcevDV.Sprint5.Task3.V18/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.MalcevDV.Sprint5.Task3.V18.Lib;
+void PrintCenteredLine(string text, int totalWidth)
+{
+    var padding = totalWidth - text.Length - 2; // -2 для звездочек по бокам
+    Console.WriteLine($"* {text}{new string(' ', padding)}*");
+}
+
+var width = 75;
+
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("Спринт #5", width);
+PrintCenteredLine("Тема: Создание решения по спринту", width);
+PrintCenteredLine("Задание #3", width);
+PrintCenteredLine("Вариант #18", width);
+PrintCenteredLine("Выполнил: Мальцев Данил Вячеславович | РППБ-25-1", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("УСЛОВИЕ:", width);
+PrintCenteredLine("Записать результат функции в бинарный файл", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+int x = 2;
+DataService ds = new DataService();
+Console.WriteLine("x = " + x);
+string res = ds.SaveToFileTextData(x);
+
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
+
+Console.WriteLine(new string('*', width));
